ea48d797cf40b63ea24de849ceb0580733b4a4f7
kk <min123@example.com>
2020-08-04 01:30:23 +0900

    메인프래그먼트 구성중

--- app/src/main/java/com/cos/brunch/MainFrag4.java
+++ app/src/main/java/com/cos/brunch/MainFrag4.java
@@ -1,6 +1,7 @@
 package com.cos.brunch;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,11 +12,19 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class MainFrag4 extends Fragment {
+    private static final String TAG = "MainFrag4";
+    
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.frag4_main, container, false);
 
+        TextView tvTitle1 = view.findViewById(R.id.tv_title1);
+        TextView tvTitle2 = view.findViewById(R.id.tv_title2);
+        tvTitle1.setText("동적변경1");
+        tvTitle2.setText("동적변경2");
+
+        Log.d(TAG, "MainFrag4 : onCreateView: ");
         return view;
     }
 
--- app/src/main/java/com/cos/brunch/MainFragmentAdapter.java
+++ app/src/main/java/com/cos/brunch/MainFragmentAdapter.java
@@ -1,5 +1,7 @@
 package com.cos.brunch;
 
+import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -13,36 +15,43 @@ import java.util.List;
 
 public class MainFragmentAdapter extends FragmentStateAdapter {
 
+    private static final String TAG = "MainFragmentAdapter";
     private List<Fragment> fragmentList = new ArrayList<>();
     // 오브젝트 데이터 (배열)
 
     public MainFragmentAdapter(@NonNull FragmentActivity fragmentActivity) {
         super(fragmentActivity);
+        Log.d(TAG, "MainFragmentAdapter: ");
     }
 
     // FragmentList에 아이템 추가하는 함수 필요
     public void addFragment(Fragment fragment){
         fragmentList.add(fragment);
+        Log.d(TAG, "MainFragmentAdapter : addFragment: " + fragment);
     }
 
     // 프레그먼트에서 뷰 찾는법 확인하여 타이틀에 뿌리기 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!(오류남 !!!!!!)
     @NonNull
     @Override
     public Fragment createFragment(int position) {
-        // 3번일 떄만 2개로
+        // 3번일 때만 2개로
         // 나머지는 그대로
-        View v = fragmentList.get(position).getView();
-        if(position == 3){
-            TextView tvTitle1 = v.findViewById(R.id.tv_title);
-            TextView tvTitle2 = v.findViewById(R.id.tv_title2);
-            tvTitle1.setText("동적변경1");
-            tvTitle2.setText("동적변경2");
-        }
+        Fragment fm = fragmentList.get(position);
+//        View v = fm.getView();
+        Log.d(TAG, "createFragment: position : " + fm);
+//        Log.d(TAG, "createFragment: " + v);
+//        if(position == 3){
+//            TextView tvTitle1 = v.findViewById(R.id.tv_title1);
+//            TextView tvTitle2 = v.findViewById(R.id.tv_title2);
+//            tvTitle1.setText("동적변경1");
+//            tvTitle2.setText("동적변경2");
+//        }
         return fragmentList.get(position);
     }
 
     @Override
     public int getItemCount() {
+        Log.d(TAG, "getItemCount: " + fragmentList.size());
         return fragmentList.size();
     }
 }
