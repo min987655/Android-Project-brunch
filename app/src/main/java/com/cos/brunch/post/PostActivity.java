404af63c1aeee6c934143c4c5ef7ac225aa22a8b
kk <min123@example.com>
2020-08-12 02:43:23 +0900

    글상세수정중

--- app/src/main/java/com/cos/brunch/post/PostActivity.java
+++ app/src/main/java/com/cos/brunch/post/PostActivity.java
@@ -1,16 +1,86 @@
 package com.cos.brunch.post;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.cos.brunch.R;
+import com.cos.brunch.model.Post;
+import com.cos.brunch.now.NowActivity;
+import com.cos.brunch.posts.PostsActivity;
+import com.cos.brunch.posts.PostsAdapter;
 
 public class PostActivity extends AppCompatActivity {
+
+    private static final String TAG = "PostActivity";
+    private Context mContext = PostActivity.this;
+    private PostAdapter postAdapter;
+    private RecyclerView rvPostPost;
+
+    private ImageView imgBack, imgSearch;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        initObject();
+        initDesign();
+        initData();
+        initlistener();
+    }
+
+    private void initObject() {
+        imgBack = findViewById(R.id.img_toolbar_l);
+        imgSearch = findViewById(R.id.img_toolbar_r);
+
+        imgBack.setImageDrawable(getDrawable(R.drawable.img_back_b));
+        imgSearch.setImageDrawable(null);
+    }
+
+    private void initDesign() {
+        rvPostPost = findViewById(R.id.rv_post_post);
+    }
+
+    private void initData() {
+        postAdapter = new PostAdapter();
+        postAdapter.addPost(new Post("","평화로운 주말 스케치",R.drawable.img_apply_profile3, "세 달쯤 전부터 TV 리모컨의 왼쪽 방향키가 잘 눌리지 않았다. 말하자면 유튜브 메인화면에서"));
+        postAdapter.addPost(new Post("","평화로운 주말 스케치",R.drawable.img_apply_profile3, "세 달쯤 전부터 TV 리모컨의 왼쪽 방향키가 잘 눌리지 않았다. 말하자면 유튜브 메인화면에서"));
+        postAdapter.addPost(new Post("","평화로운 주말 스케치",R.drawable.img_apply_profile3, "세 달쯤 전부터 TV 리모컨의 왼쪽 방향키가 잘 눌리지 않았다. 말하자면 유튜브 메인화면에서"));
+        postAdapter.addPost(new Post("","평화로운 주말 스케치",R.drawable.img_apply_profile3, "세 달쯤 전부터 TV 리모컨의 왼쪽 방향키가 잘 눌리지 않았다. 말하자면 유튜브 메인화면에서"));
+        postAdapter.addPost(new Post("","평화로운 주말 스케치",R.drawable.img_apply_profile3, "세 달쯤 전부터 TV 리모컨의 왼쪽 방향키가 잘 눌리지 않았다. 말하자면 유튜브 메인화면에서"));
+        postAdapter.addPost(new Post("","평화로운 주말 스케치",R.drawable.img_apply_profile3, "세 달쯤 전부터 TV 리모컨의 왼쪽 방향키가 잘 눌리지 않았다. 말하자면 유튜브 메인화면에서"));
+        postAdapter.addPost(new Post("","평화로운 주말 스케치",R.drawable.img_apply_profile3, "세 달쯤 전부터 TV 리모컨의 왼쪽 방향키가 잘 눌리지 않았다. 말하자면 유튜브 메인화면에서"));
+        postAdapter.addPost(new Post("","평화로운 주말 스케치",R.drawable.img_apply_profile3, "세 달쯤 전부터 TV 리모컨의 왼쪽 방향키가 잘 눌리지 않았다. 말하자면 유튜브 메인화면에서"));
+        postAdapter.addPost(new Post("","평화로운 주말 스케치",R.drawable.img_apply_profile3, "세 달쯤 전부터 TV 리모컨의 왼쪽 방향키가 잘 눌리지 않았다. 말하자면 유튜브 메인화면에서"));
+        postAdapter.addPost(new Post("","평화로운 주말 스케치",R.drawable.img_apply_profile3, "세 달쯤 전부터 TV 리모컨의 왼쪽 방향키가 잘 눌리지 않았다. 말하자면 유튜브 메인화면에서"));
+
+        rvPostPost.setLayoutManager(new LinearLayoutManager(this));
+        rvPostPost.setAdapter(postAdapter);
+    }
+
+    private void initlistener() {
+        imgBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, PostActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        postAdapter.setOnItemClickListener(new PostAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View v, int position) {
+                Intent intent = new Intent(mContext, PostActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/cos/brunch/post/PostAdapter.java
@@ -0,0 +1,96 @@
+package com.cos.brunch.post;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.cos.brunch.R;
+import com.cos.brunch.model.Post;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.MyViewHolder> {
+
+    private static final String TAG = "PostAdapter";
+    private List<Post> posts = new ArrayList<>();
+
+    // OnItemClickListener 리스너 객체 참조를 어댑터에 전달하는 메서드
+    private static OnItemClickListener mListener = null;
+
+    public interface OnItemClickListener {
+        void onItemClick(View v, int position) ;
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.mListener = listener ;
+    }
+
+    public void addPost(Post post) {
+        posts.add(post);
+    }
+
+    // 껍데기 생성
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d(TAG, "onCreateViewHolder: ");
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.item_post_detail, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    // 껍데기에 데이터 바인등
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, final int position) {
+        Post post = posts.get(position);
+        Log.d(TAG, "onBindViewHolder: " + post);
+        holder.setItem(post);
+    }
+
+    @Override
+    public int getItemCount() {
+        Log.d(TAG, "getItemCount: " + posts.size());
+        return posts.size();
+    }
+
+    // 인플레이터된 데이터 들어갈 뷰홀더
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView tvPostDetailTitle, tvPostDetailNickname;
+        private ImageView ivPostItemBackground;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvPostDetailTitle = itemView.findViewById(R.id.tv_post_detail_title);
+            tvPostDetailNickname = itemView.findViewById(R.id.tv_post_detail_nickname);
+            ivPostItemBackground = itemView.findViewById(R.id.iv_post_item_background);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.d(TAG, "onClick: 아이템 클릭 됨 ");
+                    int position = getAdapterPosition() ;
+                    if (position != RecyclerView.NO_POSITION) {
+                        mListener.onItemClick(v, position);
+                    }
+                }
+            });
+
+        }
+
+        public void setItem(Post post) {
+            Log.d(TAG, "setItem: ");
+            tvPostDetailTitle.setText(post.getTitle());
+            tvPostDetailNickname.setText("작가닉네임");
+            ivPostItemBackground.setImageResource(post.getCoverImg());
+        }
+    }
+
+}
